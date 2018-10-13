6581628240e685448860f7e25077b2bbd89e042d
Boong <Nhiem.Ng@gmail.com>
2018-10-13 16:57:34 -0500

    change landing pictures

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,71 +3,67 @@ import { Link } from 'react-router-dom'
 import { PropTypes} from 'prop-types';
 import { connect } from 'react-redux';
  class Landing extends Component {
-
-  componentDidMount(){
-    if(this.props.auth.isAuthenticated){
-      this.props.history.push('/');
-    }
+ componentDidMount(){
+  if(this.props.auth.isAuthenticated){
+   this.props.history.push('/dashboard');
   }
+ }
+ render() {
+  return (
+    <div className="landing">
+    <div className="dark-overlay landing-inner text-light">
+     <div className="container">
+      <div className="row">
+       <div className="col-md-12 text-center">
 
-  render() {
-    return (
-        <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-              
-                <h1 className="display-6 mx-auto">Welcome to Immigrant social
-                </h1>
-                <p className="lead1"> Welcome Newcomers to St.Louis's community</p>
-                <hr />
-                {/* <Link to="/register" className="btn btn-lg btn-dark ">Sign Up</Link>
-                <Link to="/login" className="btn btn-lg btn-light">Login</Link> */}
-                <p> In this Community, we connect, share ideas and help others out by sharing ideas, documents </p>
+        <h1 className="display-6 mx-auto">Immigrant Portal for St. Louis
+        </h1>
+        <p className="lead1"> Helping immgrants transitioning into new home </p>
+        <hr />
+        {/* <Link to="/register" className="btn btn-lg btn-dark ">Sign Up</Link>
+        <Link to="/login" className="btn btn-lg btn-light">Login</Link> */}
+        <p> In this Community, we connect and help others out by sharing ideas, best practices </p>
 <div className="card-deck">
-  <Link to ='/login' className="card">
-    <img className="card-img-top" src="https://images.unsplash.com/photo-1534818113099-dbe2b2e800ae?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fafe7536d18fede71e9100625feb695c&auto=format&fit=crop&w=1350&q=80" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title text-white">Card title</h5>
-      <p className="card-text text-white">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text text-white"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </Link>
-  <div className="card">
-    <img className="card-img-top" src="https://c.o0bg.com/rf/image_835w/Boston/2011-2020/2017/02/16/BostonGlobe.com/Politics/Images/635145662.jpg" alt="Card image cap"/>
-    <div className="card-body1">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
+ <div className="card">
+  <img className="card-img-top" src="https://unsplash.com/photos/NYSt8bgmPM8" alt="Card image cap"/>
+  <div className="card-body">
+   <h5 className="card-title">Housing</h5>
+   <p className="card-text">Assist with affordable housing programs enrollment, ITIN mortgages, etc </p>
+   {/* /* <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
   </div>
-  <div className="card">
-    <img className="card-img-top" src=".../100px200/" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title text-white">Card title</h5>
-      <p className="card-text text-white">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text text-white"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
+ </div>
+ <div className="card">
+  <img className="card-img-top" src="https://images.unsplash.com/photo-1501516069922-a9982bd6f3bd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=794e5cf2083ae47e801eef74ec9842cf&auto=format&fit=crop&w=1350&q=80" alt="Card image cap"/>
+  <div className="card-body1">
+   <h5 className="card-title">Employment</h5>
+   <p className="card-text">Vocational Training, Apprenticeship, Job Placement</p>
+   {/* /* <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
   </div>
+ </div>
+ <div className="card">
+  <img className="card-img-top" src="https://unsplash.com/photos/GJao3ZTX9gU" alt="Card image cap"/>
+  <div className="card-body">
+   <h5 className="card-title">Legal Services</h5>
+   <p className="card-text">Adjustment of Status, Consular Processing, Deferred Action for Childhood Arrivals (DACA), Employment authorization, Family-based petitions, Naturalization/Citizenship, Temporary Protected Status (TPS), U visas, Violence Against Women Act (VAWA) petitions</p>
+   {/* /*<p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
+  </div>
+ </div>
 </div>
-
-            </div>
-          </div>
-        </div>
       </div>
+     </div>
     </div>
-    
-    
-    )
-    
-  }
+   </div>
+  </div>
+
+
+  )
+
+ }
 }
 Landing.propTypes = {
-  auth: PropTypes.object.isRequired,
+ auth: PropTypes.object.isRequired,
 }
-
 const mapStateToProps = (state) => ({
-  auth: state.auth
+ auth: state.auth
 })
 export default connect(mapStateToProps)(Landing);
