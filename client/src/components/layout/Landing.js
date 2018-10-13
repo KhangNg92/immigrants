182f398548b541eb8454ca34179eb9e483659b2b
Boong <Nhiem.Ng@gmail.com>
2018-10-13 17:19:38 -0500

    update landing and layout

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -22,10 +22,10 @@ import { connect } from 'react-redux';
         <hr />
         {/* <Link to="/register" className="btn btn-lg btn-dark ">Sign Up</Link>
         <Link to="/login" className="btn btn-lg btn-light">Login</Link> */}
-        <p> In this Community, we connect and help others out by sharing ideas, best practices </p>
+        <p> In this Community, we connect and help others out by sharing ideas and best practices </p>
 <div className="card-deck">
  <div className="card">
-  <img className="card-img-top" src="https://unsplash.com/photos/NYSt8bgmPM8" alt="Card image cap"/>
+  <img className="card-img-top" src="https://images.unsplash.com/photo-1516156008625-3a9d6067fab5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=47b4b88e2825332da6fb93898562051c&auto=format&fit=crop&w=1050&q=80" alt="Card image cap"/>
   <div className="card-body">
    <h5 className="card-title">Housing</h5>
    <p className="card-text">Assist with affordable housing programs enrollment, ITIN mortgages, etc </p>
@@ -41,10 +41,10 @@ import { connect } from 'react-redux';
   </div>
  </div>
  <div className="card">
-  <img className="card-img-top" src="https://unsplash.com/photos/GJao3ZTX9gU" alt="Card image cap"/>
+  <img className="card-img-top" src="https://images.unsplash.com/photo-1521791055366-0d553872125f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8428c213c5c30918e7a4b10cb37d482f&auto=format&fit=crop&w=1350&q=80" alt="Card image cap"/>
   <div className="card-body">
    <h5 className="card-title">Legal Services</h5>
-   <p className="card-text">Adjustment of Status, Consular Processing, Deferred Action for Childhood Arrivals (DACA), Employment authorization, Family-based petitions, Naturalization/Citizenship, Temporary Protected Status (TPS), U visas, Violence Against Women Act (VAWA) petitions</p>
+   <p className="card-text">Employment authorization, Family-based petitions, Naturalization/Citizenship</p>
    {/* /*<p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> */}
   </div>
  </div>
